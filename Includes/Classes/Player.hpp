d62da170d6a4205d2e756e5175c6269acad6c98a
Sarah Rabelo Araujo <117447148+LoiraDoTchan@users.noreply.github.com>
2023-07-04 20:16:19 -0300

    Update Player.hpp adicionando uma nova classe
    Neste hpp, eu adicionei uma classe duplicada da classe Player com a finalidade de adicionar um novo jogador, porem de uma maneira que mude as teclas para movimenta-lo e os sprites.

--- Includes/Classes/Player.hpp
+++ Includes/Classes/Player.hpp
@@ -91,4 +91,91 @@ public: // Funcoes publicas
 	}
 };
 
+
+class Player1: public GameObject {
+private: // Variaveis privadas da classe Player
+	sf::Texture playerTexture1;
+	sf::Vector2f previousPosition1;
+
+	float playerSpeed1;
+	float deltaTime = 0;
+
+private: // Funcoes privadas da classe Player
+	void loadTextures1() {
+		if (!playerTexture1.loadFromFile("Assets/Characters/Females/F_01.png")) {
+			std::cerr << "Falha ao carregar textura do jogador!";
+		} else {
+			playerTexture1.loadFromFile("Assets/Characters/Females/F_01.png");
+		}
+	}
+
+	void setSprites1() {
+		objectSprite1.setScale(sf::Vector2f(2.0f, 2.0f));
+		objectSprite1.setTexture(playerTexture1);
+		objectSprite1.setTextureRect(sf::IntRect(2, 2, 12, 15));
+	}
+
+	void handlePlayerInput1() {
+		sf::Vector2f movement1(0.0f, 0.0f);
+
+		if (sf::Keyboard::isKeyPressed(Keyboard::Up)) {
+			movement1.y -= 1.0f;
+			objectSprite1.setTextureRect(sf::IntRect(34, 2, 12, 15));
+		}
+
+		if (sf::Keyboard::isKeyPressed(Keyboard::Down)) {
+			movement1.y += 1.0f;
+			objectSprite1.setTextureRect(sf::IntRect(2, 2, 12, 15));
+		}
+
+		if (sf::Keyboard::isKeyPressed(Keyboard::Left)) {
+			movement1.x -= 1.0f;
+			objectSprite1.setTextureRect(sf::IntRect(52, 2, 9, 15));
+		}
+
+		if (sf::Keyboard::isKeyPressed(Keyboard::Right)) {
+			movement1.x += 1.0f;
+			objectSprite1.setTextureRect(sf::IntRect(19, 2, 9, 15));
+		}
+
+		moveCharacter1(movement1);
+
+	}
+
+	void updateDeltaTime1(float dt) {
+		deltaTime = dt;
+	}
+
+	void moveCharacter1(const sf::Vector2f direction) {
+		previousPosition1 = objectSprite1.getPosition();
+		sf::Vector2f movement1 = direction * playerSpeed1 * deltaTime;
+		objectSprite.move(movement1);
+	}
+
+	void setPreviousPosition1() {
+		objectSprite1.setPosition(previousPosition1);
+	}
+
+	bool isCollidingWithMaze1(Maze &maze) {
+		 if(maze.checkCollision(objectSprite1) == true) {
+			 setPreviousPosition1();
+		 }
+
+		 return maze.checkCollision(objectSprite1);
+	}
+
+public: // Funcoes publicas
+	Player1() :
+			playerSpeed1(150.0f) {
+		loadTextures1();
+		setSprites1();
+	}
+
+	void updatePlayer1(float deltaTime, Maze &maze) {
+		handlePlayerInput1();
+		isCollidingWithMaze1(maze);
+		updateDeltaTime1(deltaTime);
+	}
+};
+
 #endif
