b4a0139b0e6b655a13594a43baf56b3ec5554ca0
Sarah Rabelo Araujo <117447148+LoiraDoTchan@users.noreply.github.com>
2023-07-04 20:13:48 -0300

    Update main.cpp para dois players
    Adicionei um objeto referente a nova classe Player1, no qual eu utilizo para ter outro jogador. Dupliquei a classe Player, modificando somente o nome das variaveis, os botões para movimentar o personagem e o sprite.

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@ int main() {
 		gameWindow.setIcon(image.getSize().x, image.getSize().y, image.getPixelsPtr());
 
 	Player player1;
+	Player1 player2;
+
 	Maze maze(gameWindow);
 
 	while (gameWindow.isOpen()) {
@@ -29,6 +31,9 @@ int main() {
 		maze.drawOnWindow(gameWindow);
 		player1.updatePlayer(deltaTime, maze);
 		player1.drawOnWindow(gameWindow);
+
+		player2.updatePlayer1(deltaTime, maze);
+		player2.drawOnWindow(gameWindow);
 		gameWindow.display();
 	}
 
